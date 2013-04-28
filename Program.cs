d5074e6353399aff6c394c740efeee9edeffbd87
James Dunne <james.jdunne@gmail.com>
2013-04-28 16:10:06 -0500

    Initial commit of LANCaster for PGM.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LANCaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
